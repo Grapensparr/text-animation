46ff2156193f70a582fb861c17dd2b9d2fb3b178
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-01-21 21:54:25 +0100

    Updated animation

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+//To get the correct stroke-dasharray
 const hello = document.querySelectorAll('.hello path')
-
-console.log(hello)
 
 for (let i = 0; i<hello.length; i++){
     console.log(`Letter ${i} is ${hello[i].getTotalLength()}`);
